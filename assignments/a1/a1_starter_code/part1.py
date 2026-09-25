a78c071519764a46895da2f52ca1364daf52c869
SuperChute <john.galang102@gmail.com>
2026-09-25 14:08:57 -0600

    Simulated Annealing for 60% average decode rate

--- assignments/a1/a1_starter_code/part1.py
+++ assignments/a1/a1_starter_code/part1.py
@@ -19,24 +19,70 @@ rng = np.random.default_rng()
 settings = rng.random(size=2)
 #settings = [0.2, 0.7]
 
-reward = 0
-translated_string = translator.translate(settings)
-segments = translated_string.split()
+def func_wrapper(settings): 
+  translated_string = translator.translate(settings)
+  segments = translated_string.split()
 
-for code in segments: 
-  if code.isalpha(): 
-    reward += 1  
+  reward = 0
+  for code in segments: 
+    if code.isalpha(): 
+      reward += 1   
 
-print(f"{segments}\n")
+  decode_rate = reward / len(segments) 
 
-print(f"Settings: {settings}")
+  return -decode_rate
 
-#Item Count
-print(f"Amount of Segmented Words (Constant): {len(segments)}")
+def sim_annealing(f, settings, n=100, T=1000): 
+  current = settings 
+  multiplier = 0.95
+  best_rate = 0
+  best_settings = [0,0]
 
-#Character Count
-print(f"Amount of characters {len(translated_string)}") 
+  for _ in range(n): 
+    T *= multiplier 
 
-print(f"Amount of words decoded from Segment {reward}")
+    #flip coin, when adding to choice, must be within 0 - 1 bounds
+    choice = np.random.rand(len(settings)) * 0.09 # makes the numbers smaller so that they don't overshoot the 0 - 1 bound
+    if np.random.random() > .50:
+      next = current - choice
+    else: 
+      next = current + choice  
+    #Ensures that the bounds for each setting stay within 0 - 1
+    next = np.clip(next, 0.0, 1.0)
 
-print(f"Percentage of Words Decoded: {(reward / len(segments)) * 100}")
+    f_next = f(next)
+
+    if f_next < best_rate: 
+      best_rate = f_next
+      best_settings = next
+  
+    f_curr = f(current) 
+    #Multiplying delta_e by 100 since without it would be a very small number, causing calculations to always accept bad settings
+    delta_e = (f_next - f_curr) * 100
+
+    if delta_e < 0: 
+      current = next 
+      best_rate = f_next 
+    elif np.random.random() < np.exp(((-delta_e)/T)): 
+      current = next  
+
+  return(best_settings, best_rate) 
+
+settings, rate = sim_annealing(func_wrapper, settings=rng.random(size=2), T=1000)
+print(translator.translate(settings))
+print(settings)
+print(abs(rate) * 100)
+
+# print(f"{segments}\n")
+
+# print(f"Settings: {settings}")
+
+# #Item Count
+# print(f"Amount of Segmented Words (Constant): {len(segments)}")
+
+# #Character Count
+# print(f"Amount of characters {len(translated_string)}") 
+
+# print(f"Amount of words decoded from Segment {reward}")
+
+# print(f"Percentage of Words Decoded: {(reward / len(segments)) * 100}")
